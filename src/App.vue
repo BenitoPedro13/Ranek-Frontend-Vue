94bb0931a0738262bf301e9d672cb1ad0852d464
1REZ3 <benitopedro21@outlook.com>
2021-07-11 02:18:46 -0300

    created login page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,125 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <the-header></the-header>
+    <main>
+      <transition mode="out-in">
+        <router-view/>
+      </transition>
+    </main>
+    <the-footer></the-footer>
   </div>
 </template>
 
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+  * {
+    box-sizing: border-box;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  body, ul, li, h1, h2, p {
+    padding: 0;
+    margin: 0;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  ul {
+    list-style: none;
+  }
+
+  body {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: #345;
+    background: url('./assets/pattern.svg') repeat top;
+  }
+
+  a {
+    color: #345;
+    text-decoration: none;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+
+  .btn {
+    display: block;
+    padding: 10px 30px;
+    background: #87f;
+    border-radius: 4px;
+    color: #fff;
+    text-align: center;
+    font-size: 1rem;
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+    transition: all .3s;
+    border: none;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #65d;
+    transform: scale(1.1);
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  main {
+    flex: 1;
+  }
+
+  label {
+    margin-bottom: 5px;
+  }
+
+  input, textarea {
+    border-radius: 4px;
+    border: 1px solid #fff;
+    padding: 15px;
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+    transition: all 0.3s;
+    font-size: 1rem;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    margin-bottom: 15px;
+  }
+
+  input:hover,
+  input:focus,
+  textarea:hover,
+  textarea:focus {
+    outline: none;
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+    border-color: #87f;
+  }
+
+  .v-enter, v-leave-to {
+    opacity: 0;
+  }
+
+  .v-enter {
+    transition: translate3d(0, -20px, 0);
+  } 
+
+  .v-leave-to {
+    transition: translate3d(0, 20px, 0);
+  }
+
+  .v-enter-active, v-leave-active {
+    transition: all .3s;
+  }
 </style>
