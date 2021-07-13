740e1e92a818613cc7aad8cfcc6577716a85fbb7
1REZ3 <benitopedro21@outlook.com>
2021-07-13 08:15:01 -0300

    criadas paginas de compras e vendas do usuario

--- src/views/usuario/UsuarioCompras.vue
+++ src/views/usuario/UsuarioCompras.vue
@@ -1,13 +1,68 @@
 <template>
-  <p>compras</p>
+  <div v-if="compras">
+    <h2>Compras</h2>
+    <div class="produtos-wrapper" v-for="(compra, index) in compras" :key="index">
+      <produtos-item v-if="compra.produto" :produto="compra.produto">
+        <p class="vendedor">
+          <span>Vendedor: </span> {{compra.vendedor_id}}
+        </p>
+      </produtos-item>
+    </div>
+  </div>
 </template>
 
 <script>
+import ProdutosItem from '@/components/ProdutosItem.vue'
+import {api} from '@/services.js'
+import { mapState } from 'vuex'
+
 export default {
-  name: 'UsuarioCompras'
+  name: 'UsuarioCompras',
+  data() {
+    return {
+      compras: null,
+    }
+  },
+  components: {
+    ProdutosItem
+  },
+  methods: {
+    async getCompras() {
+      const comprasJson = await api.get(`/transacao?comprador_id=${this.usuario.id}`)
+      this.compras = comprasJson.data
+    },
+    created() {
+      this.getCompras()
+    }
+  },
+  computed: {
+    ...mapState(['usuario', 'login'])
+  },
+  watch: {
+    login() {
+      this.getCompras()
+    }
+  },
+  created() {
+    if(this.login){
+      this.getCompras()
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80
+}
+
+h2 {
+  margin-bottom: 20px;
+}
 
 </style>
--- src/views/usuario/UsuarioVendas.vue
+++ src/views/usuario/UsuarioVendas.vue
@@ -1,13 +1,85 @@
 <template>
-  <p>vendas</p>
+  <div v-if="vendas">
+    <h2>Vendas</h2>
+    <div class="produtos-wrapper" v-for="(venda, index) in vendas" :key="index">
+      <produtos-item v-if="venda.produto" :produto="venda.produto">
+        <p class="comprador">
+          <span>Comprador: </span> {{venda.comprador_id}}
+        </p>
+      </produtos-item>
+      <div class="entrega">
+        <h3>Entrega:</h3>
+        <ul v-if="venda.endereco">
+          <li v-for="(value, key) in venda.endereco" :key="key">
+            {{key}}: {{value}}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ProdutosItem from '@/components/ProdutosItem.vue'
+import {api} from '@/services.js'
+import { mapState } from 'vuex'
+
 export default {
   name: 'UsuarioVendas',
+  data() {
+    return {
+      vendas: null,
+    }
+  },
+  components: {
+    ProdutosItem
+  },
+  methods: {
+    async getVendas() {
+      const vendasJson = await api.get(`/transacao?vendedor_id=${this.usuario.id}`)
+      this.vendas = vendasJson.data
+    }
+  },
+  computed: {
+    ...mapState(['usuario', 'login'])
+  },
+  watch: {
+    login() {
+      this.getVendas()
+    }
+  },
+  created() {
+    if(this.login){
+      this.getVendas()
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.comprador span {
+  color: #e80
+}
+
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom: 60px;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+.h3 {
+  justify-self: end;
+  margin: 0;
+}
 
 </style>
